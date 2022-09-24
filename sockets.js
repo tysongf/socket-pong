0143a9770bdeeea12737dc91a9993754e4e2adc1
Tyson G Fritz <tysongfritz@gmail.com>
2022-09-24 00:13:27 -0700

    Implement socket.io rooms for multiple game sessions per server.

--- sockets.js
+++ sockets.js
@@ -3,31 +3,33 @@ let numPlayersReady = 0;
 function listen(io) {
    const pongNamespace = io.of("/pong");
    pongNamespace.on("connection", (socket) => {
-      console.log(`User Connected: ${socket.id}`);
+      let room;
 
       socket.on("ready", () => {
-         console.log(`Player ready: ${socket.id}`);
+         room = "room_" + Math.floor(numPlayersReady / 2); //room_0, room_1, room_2, etc
+         socket.join(room);
+
+         console.log("Player ready", socket.id, room);
+
          numPlayersReady++;
-         if (numPlayersReady == 2) {
-            //second player assigned as host.
-            // emit startGame to all connected clients.
-            pongNamespace.emit("startGame", socket.id);
-            numPlayersReady = 0;
+         if (numPlayersReady % 2 == 0) {
+            // player 2 assigned as host.
+            // emit startGame to connected clients in the room.
+            pongNamespace.in(room).emit("startGame", socket.id);
          }
       });
 
       socket.on("paddleMove", (paddleData) => {
-         //broadcast to all clients except the emitter
-         //ie. broadcast to the other player
-         socket.broadcast.emit("paddleMove", paddleData);
+         socket.to(room).emit("paddleMove", paddleData);
       });
 
       socket.on("ballMove", (ballData) => {
-         socket.broadcast.emit("ballMove", ballData);
+         socket.to(room).emit("ballMove", ballData);
       });
 
       socket.on("disconnect", (reason) => {
-         socket.broadcast.emit();
+         console.log(`Client ${socket.id} disconnected: ${reason}`);
+         socket.leave(room);
       });
    });
 }
